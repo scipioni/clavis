b88c8639896887a4d81b8fe84d8ad8d8f40ce1ac
Stefano Scipioni <scipio.it@gmail.com>
2022-04-19 15:42:19 +0200

    qmk cleanup

--- qmk/clavis/rules.mk
+++ qmk/clavis/rules.mk
@@ -1,9 +1,9 @@
 
 SPLIT_KEYBOARD = yes
-#RGBLIGHT_ENABLE = no
-RGB_MATRIX_ENABLE = yes
-OLED_ENABLE = yes
-OLED_DRIVER = SSD1306
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
+OLED_ENABLE = no
+#OLED_DRIVER = SSD1306
 VIA_ENABLE = no
 LTO_ENABLE = yes # smaller size
 
@@ -12,15 +12,5 @@ EXTRAKEY_ENABLE = yes # Audio control and System control
 AUTO_SHIFT_ENABLE = yes # Auto Shift
 
 # https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
-EXTRAFLAGS += -flto
+#EXTRAFLAGS += -flto
 
-
-# If you want to change the display of OLED, you need to change here
-        # SRC +=  ./lib/glcdfont.c \
-        #         ./lib/rgb_state_reader.c \
-        #         ./lib/layer_state_reader.c \
-        #         ./lib/logo_reader.c \
-        #         ./lib/keylogger.c \
-        #         # ./lib/mode_icon_reader.c \
-        #         # ./lib/host_led_state_reader.c \
-#         # ./lib/timelogger.c \
