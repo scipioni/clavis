152fcd650c820f4caa39d2ee53aaf22c88456577
Stefano Scipioni <scipio.it@gmail.com>
2025-01-26 18:07:45 +0100

    fix: single quote and double quote

--- firmware/zmk/config/custom_config.h
+++ firmware/zmk/config/custom_config.h
@@ -4,3 +4,10 @@ U_NA,          &kp N7,       &kp N8,       &kp N9,      U_NA,              U_RGB
 U_NA,          &kp N4,       &kp N5,       &kp N6,      U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 &kp N0,        &kp N1,       &kp N2,       &kp N3,      &kp KP_DOT,        &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 U_NP,          U_NP,         U_NA,         U_NA,        U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
+
+
+#define MIRYOKU_LAYER_NUM \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SQT,           &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp GRAVE,         &kp N1,            &kp N2,            &kp N3,            &kp BSLH,          U_NA,              &kp RA(SQT),       &kp RA(DQT),     &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
