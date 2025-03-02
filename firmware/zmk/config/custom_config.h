bd4421b49cd258e1f1bc069161c24ffc87308171
Stefano Scipioni <scipio.it@gmail.com>
2025-03-02 18:50:09 +0100

    numpad + and -

--- firmware/zmk/config/custom_config.h
+++ firmware/zmk/config/custom_config.h
@@ -1,7 +1,7 @@
 
 #define MIRYOKU_LAYER_MEDIA \
-U_NA,          &kp KP_N7,       &kp KP_N8,       &kp KP_N9,      U_NA,              U_RGB_TOG,         U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         \
-U_NA,          &kp KP_N4,       &kp KP_N5,       &kp KP_N6,      U_NA,              U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
+U_NA,          &kp KP_N7,       &kp KP_N8,       &kp KP_N9,      &kp KP_MINUS,      U_RGB_TOG,         U_RGB_EFF,         U_RGB_HUI,         U_RGB_SAI,         U_RGB_BRI,         \
+U_NA,          &kp KP_N4,       &kp KP_N5,       &kp KP_N6,      &kp KP_PLUS,       U_EP_TOG,          &kp C_PREV,        &kp C_VOL_DN,      &kp C_VOL_UP,      &kp C_NEXT,        \
 &kp KP_N0,     &kp KP_N1,       &kp KP_N2,       &kp KP_N3,      &kp KP_DOT,        &u_out_tog,        &u_bt_sel_0,       &u_bt_sel_1,       &u_bt_sel_2,       &u_bt_sel_3,       \
 U_NP,          U_NP,            U_NA,            U_NA,           U_NA,              &kp C_STOP,        &kp C_PP,          &kp C_MUTE,        U_NP,              U_NP
 
