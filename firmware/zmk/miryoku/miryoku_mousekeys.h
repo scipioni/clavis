32fa9e92dd3b03918ed8b4041a070b0a328cd0a2
Stefano Scipioni <scipio.it@gmail.com>
2025-10-03 07:07:39 +0200

    sync miryoku repo

--- firmware/zmk/miryoku/miryoku_mousekeys.h
+++ firmware/zmk/miryoku/miryoku_mousekeys.h
@@ -3,15 +3,25 @@
 
 #pragma once
 
+#define ZMK_POINTING_DEFAULT_MOVE_VAL 1250
+#define ZMK_POINTING_DEFAULT_SCRL_VAL 100
+
+#define U_MOUSE_MOVE_EXPONENT 1
+#define U_MOUSE_MOVE_TIME 1500
+#define U_MOUSE_MOVE_DELAY 0
+#define U_MOUSE_SCROLL_EXPONENT 1
+#define U_MOUSE_SCROLL_TIME 5000
+#define U_MOUSE_SCROLL_DELAY 0
+
 #define U_BTN1 &mkp MB1
 #define U_BTN2 &mkp MB2
 #define U_BTN3 &mkp MB3
 
-#define U_MS_D &kp KP_N2
-#define U_MS_L &kp KP_N4
-#define U_MS_R &kp KP_N6
-#define U_MS_U &kp KP_N8
-#define U_WH_D U_NU
-#define U_WH_L U_NU
-#define U_WH_R U_NU
-#define U_WH_U U_NU
+#define U_MS_D &mmv MOVE_DOWN
+#define U_MS_L &mmv MOVE_LEFT
+#define U_MS_R &mmv MOVE_RIGHT
+#define U_MS_U &mmv MOVE_UP
+#define U_WH_D &msc SCRL_DOWN
+#define U_WH_L &msc SCRL_LEFT
+#define U_WH_R &msc SCRL_RIGHT
+#define U_WH_U &msc SCRL_UP
